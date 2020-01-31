24e4563984117d8a33e514f0d9930fb979e3f3de
LCN29 <CanLee529@gmail.com>
2020-01-31 16:57:40 +0800

    add transaction and security base starter

---
+++ security-starter/src/main/java/com/lcn29/App.java
@@ -0,0 +1,13 @@
+package com.lcn29;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
