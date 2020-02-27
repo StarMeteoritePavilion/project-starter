4b9240b1d1c9d113cf92600e7079b5071d5ee7f4
LCN29 <CanLee529@gmail.com>
2020-02-27 15:24:10 +0800

    modify JwtService

--- security-starter/src/main/java/com/lcn29/starter/security/component/service/JwtService.java
+++ security-starter/src/main/java/com/lcn29/starter/security/component/service/JwtService.java
@@ -4,12 +4,14 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.lcn29.kit.util.CollectionMapUtil;
 import com.lcn29.starter.security.component.payload.JwtPayload;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
